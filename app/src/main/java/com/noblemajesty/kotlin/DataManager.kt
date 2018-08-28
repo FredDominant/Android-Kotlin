95b5051152f382fd7e5065c8815c099660db68a3
Fred <fredadewole@gmail.com>
2018-08-28 16:42:09 +0100

    [feature DataManager] initialize dummy courses

--- app/src/main/java/com/noblemajesty/kotlin/DataManager.kt
+++ app/src/main/java/com/noblemajesty/kotlin/DataManager.kt
@@ -2,6 +2,23 @@ package com.noblemajesty.kotlin
 
 
 class DataManager {
+    init {
+        initializeCourses()
+    }
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    private fun initializeCourses() {
+        var course = CourseInfo(title = "Android programming with Intents", courseId = "android intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("Android async", "Android programming async")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("Java Fundamental", "Java programming fundamentals")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "Kotlin Beginners", title = "Android programming in Kotlin for beginners")
+        courses.set(course.courseId, course)
+    }
 }
